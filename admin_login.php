fb807804e4b51b5e916ae8dfa19b66cf1ce4f1c8
kanishk13 <42895589+kanishk13@users.noreply.github.com>
2020-09-22 17:07:02 +0530

    Update admin_login.php

--- admin_login.php
+++ admin_login.php
@@ -1,16 +1,16 @@
 <?php
-$con=mysqli_connect("localhost","root","","localhost");
+$con=mysqli_connect("localhost","root","","localhost"); //database Connection 
     if(!$con)
-        echo "not connected in db";
+        echo "not connected in db"; //if connection is not established throw an error
 
 $email=$_POST['uname'];
 $pswd=$_POST['pswd'];
 
-$sql="INSERT INTO admin_table(email,password) VALUES ('$email','$pswd')";
+$sql="INSERT INTO admin_table(email,password) VALUES ('$email','$pswd')";  //if connection is established insert into to the table 
 
 if(mysqli_query($con,$sql))
-    echo "query executed";
+    echo "query executed"; //and show a message that table has been updated 
 else 
-    echo $con   ->error;
+    echo $con   ->error;   //if not show why it is showing that error
 
 ?>
